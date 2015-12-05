7d925b64b2507729a5184a4349913187c4218b19
Ramya Puliadi <rpuliadi@gmail.com>
2015-12-05 18:51:36 -0500

    csv reader

---
+++ genMatrix.scala
@@ -0,0 +1,15 @@
+//Reads in csv file and creates a list of list of doubles
+
+import scala.io.Source
+
+class genMatrix {
+	def csvReader(): List[List[Double]] = {
+		val matrix = Source.fromFile("test.csv")
+						.getLines.toList
+						.map(_.split(",").map(_.trim.toDouble)
+						.toList)
+		return matrix
+	}
+	
+}
+
