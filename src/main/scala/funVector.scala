f41a62688e850a1e1787e951b6a1727b20b01ca9
andrew-wm-arthur <andy.wm.arthur@gmail.com>
2015-12-13 18:06:46 -0600

    fixed summarize

--- src/main/scala/funVector.scala
+++ src/main/scala/funVector.scala
@@ -81,7 +81,7 @@ class funVector( val elems: List[Double] ) {
                   l match {
                         case Nil     => index
                         case l :: ls => {
-                              if (l > max) smrz( ls, cnt+1, cnt+1, l) 
+                              if (l >= max) smrz( ls, cnt, cnt+1, l) 
                               else         smrz( ls, index, cnt+1, max)
                         }
                   }
@@ -89,7 +89,7 @@ class funVector( val elems: List[Double] ) {
             def create( l: List[Double], index: Int, cnt: Int): List[Double] = {
                   l match {
                         case Nil => Nil
-                        case l :: ls => (if(cnt == index) 1 else 0) :: create( ls, index, cnt+1)
+                        case l :: ls => (if(index == cnt) 1 else 0) :: create( ls, index, cnt+1)
                   }
             }
             val (e :: es) = this.elems
